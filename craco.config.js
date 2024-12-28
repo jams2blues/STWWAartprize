17473edc8437e2fa7c6b11c87737ce8eb3ab5f00
jams2blues <jams2blues@gmail.com>
2024-12-28 14:01:28 -0600

    Fix Babel plugin versions and update configurations

--- craco.config.js
+++ craco.config.js
@@ -3,33 +3,25 @@
 const webpack = require('webpack');
 
 module.exports = {
-  /**
-   * 1) Add a babel section to inject Babel plugins with matching "loose" mode
-   *    This ensures all class-related proposals are consistently configured,
-   *    preventing mismatch errors, and addresses the "private-property-in-object" usage.
-   */
   babel: {
     plugins: [
-      // Keep "loose" the same for all class-related proposals
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-proposal-private-methods', { loose: true }],
       ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ],
   },
-
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
-      // 2) Replace Deprecated Webpack Dev Server Options
+      // Replace Deprecated Webpack Dev Server Options
       if (webpackConfig.devServer) {
         webpackConfig.devServer.setupMiddlewares = (middlewares, devServer) => {
-          // Add custom middlewares here if needed
           return middlewares;
         };
         delete webpackConfig.devServer.onBeforeSetupMiddleware;
         delete webpackConfig.devServer.onAfterSetupMiddleware;
       }
 
-      // 3) Polyfills for Node.js Core Modules
+      // Polyfills for Node.js Core Modules
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         stream: require.resolve('stream-browserify'),
@@ -41,7 +33,7 @@ module.exports = {
         util: require.resolve('util'),
         os: require.resolve('os-browserify/browser'),
         process: require.resolve('process/browser.js'),
-        fs: false, // Mock 'fs' as it's not available in the browser
+        fs: false,
       };
 
       // Alias 'stream' to 'stream-browserify'
@@ -59,7 +51,7 @@ module.exports = {
         }),
       ];
 
-      // 4) Suppress Source Map Warnings for @airgap Packages
+      // Suppress Source Map Warnings for @airgap Packages
       webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
         if (
           rule.use &&
@@ -70,7 +62,7 @@ module.exports = {
             ...rule,
             exclude: [
               ...(rule.exclude || []),
-              /@airgap\//, // Exclude all @airgap packages
+              /@airgap\//,
               /@airgap\/beacon-core/,
               /@airgap\/beacon-dapp/,
               /@airgap\/beacon-transport-matrix/,
@@ -82,17 +74,16 @@ module.exports = {
         return rule;
       });
 
-      // 5) Ignore Remaining Source Map Loader Warnings
+      // Ignore Remaining Source Map Loader Warnings
       webpackConfig.ignoreWarnings = [
         ...(webpackConfig.ignoreWarnings || []),
-        // Silence warnings from source-map-loader about failed source map parsing
         {
           module: /source-map-loader/,
           message: /Failed to parse source map/,
         },
       ];
 
-      // 6) Exclude Additional Problematic Modules from source-map-loader
+      // Exclude Additional Problematic Modules from source-map-loader
       webpackConfig.module.rules.push({
         test: /\.js$/,
         enforce: 'pre',
